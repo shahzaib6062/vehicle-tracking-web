5840f4d74d3112c2ce4f5d11ea4939bf1e804b2e
jahanzaib2630 <jahanzaib4587@gmail.com>
2024-01-28 02:54:57 +0500

    fixes sheet issues

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,10 +1,12 @@
 import Header from "@/component/header";
+import Users from "@/component/usersListing";
 import React from "react";
 
 const index = () => {
   return (
     <>
       <Header />
+      <Users />
     </>
   );
 };
