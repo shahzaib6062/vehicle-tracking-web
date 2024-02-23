ba4d489d393f965c20a407aefb647228ca91a89e
Abubakr Muhammad <realchampiontheno1@gmail.com>
2024-02-24 01:16:27 +0500

    add user info modal

--- src/components/usersTable.jsx
+++ src/components/usersTable.jsx
@@ -8,7 +8,6 @@ import {
   Th,
   Td,
   TableContainer,
-  Link,
   IconButton,
   HStack,
   Badge,
@@ -17,24 +16,31 @@ import {
   Button,
   Text,
   Tooltip,
-} from "@chakra-ui/react";
-import { useActiveUsers, useDisableUser, useEnableUser } from "@/lib/users";
-import DefaultSpinner from "@/components/ui/defaultSpinner";
-import {
-  CheckCircleIcon,
-  CheckIcon,
-  DeleteIcon,
-  NotAllowedIcon,
-  ViewIcon,
-} from "@chakra-ui/icons";
-import {
   AlertDialog,
   AlertDialogBody,
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogContent,
   AlertDialogOverlay,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  Stack,
+  Flex,
+  Avatar,
+  Image,
 } from "@chakra-ui/react";
+import {
+  useActiveUsers as useAllUsers,
+  useDisableUser,
+  useEnableUser,
+} from "@/lib/users";
+import DefaultSpinner from "@/components/ui/defaultSpinner";
+import { CheckCircleIcon, NotAllowedIcon, ViewIcon } from "@chakra-ui/icons";
+import { Link } from "@chakra-ui/next-js";
 
 function UserStatusConfirmationDialog({
   isOpen,
@@ -112,12 +118,90 @@ function UserStatusConfirmationDialog({
   );
 }
 
+function UserInfoModal({ isOpen, onClose, user }) {
+  const { data: users } = useAllUsers();
+  const currentUser = users.find((u) => u.uid === user?.uid);
+  const { uid, username, email, role, isActive } = currentUser || {};
+
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>User Info</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Box w={"full"} bg={"white"} rounded={"md"} overflow={"hidden"}>
+            <Image
+              h={"120px"}
+              w={"full"}
+              src={
+                "https://images.unsplash.com/photo-1563197527-b5677321c356?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80"
+              }
+              objectFit="cover"
+              alt="#"
+            />
+            <Flex justify={"center"} mt={-12}>
+              <Avatar
+                size={"xl"}
+                name={username}
+                css={{ border: "2px solid white" }}
+                bg={"cyan.200"}
+                color={"cyan.800"}
+              />
+            </Flex>
+
+            <Box p={6}>
+              <Stack spacing={0} align={"center"} mb={5}>
+                <Heading fontSize={"2xl"} fontWeight={500}>
+                  {username}
+                </Heading>
+                <Text color={"gray.500"}>{email}</Text>
+              </Stack>
+
+              <Stack direction={"row"} justify={"center"} spacing={10}>
+                <Stack spacing={1} align={"center"}>
+                  <Text fontSize={"sm"} color={"gray.500"}>
+                    Status
+                  </Text>
+
+                  <Badge colorScheme={isActive ? "green" : "yellow"}>
+                    {isActive ? "Active" : "Inactive"}
+                  </Badge>
+                </Stack>
+                <Stack spacing={1} align={"center"}>
+                  <Text fontSize={"sm"} color={"gray.500"}>
+                    Role
+                  </Text>
+                  <Badge colorScheme={role === "admin" ? "cyan" : "gray"}>
+                    {role === "admin" ? "Admin" : "User"}
+                  </Badge>
+                </Stack>
+              </Stack>
+
+              <Link href={`/admin/users/${uid}`}>
+                <Button w={"full"} mt={8} rounded={"md"} colorScheme={"blue"}>
+                  Go to Details
+                </Button>
+              </Link>
+            </Box>
+          </Box>
+        </ModalBody>
+      </ModalContent>
+    </Modal>
+  );
+}
+
 export default function UsersTable() {
-  const { data: users, isLoading } = useActiveUsers();
+  const { data: users, isLoading } = useAllUsers();
   const {
     isOpen: isStatusChangeOpen,
     onClose: onStatusChangeClose,
     onOpen: onStatusChangeOpen,
+  } = useDisclosure();
+  const {
+    isOpen: isUserInfoOpen,
+    onClose: onUserInfoClose,
+    onOpen: onUserInfoOpen,
   } = useDisclosure();
   const [activeUser, setActiveUser] = useState(null);
   const [action, setAction] = useState("disable");
@@ -140,6 +224,16 @@ export default function UsersTable() {
     onStatusChangeClose();
   };
 
+  const openUserInfoModal = (user) => {
+    setActiveUser(user);
+    onUserInfoOpen();
+  };
+
+  const closeUserInfoModal = () => {
+    setActiveUser(null);
+    onUserInfoClose();
+  };
+
   if (isLoading) return <DefaultSpinner />;
 
   return (
@@ -151,6 +245,12 @@ export default function UsersTable() {
         onClose={cancelStatusChange}
         user={activeUser}
         action={action}
+      />
+
+      <UserInfoModal
+        isOpen={isUserInfoOpen}
+        onClose={closeUserInfoModal}
+        user={activeUser}
       />
 
       <TableContainer mt="6">
@@ -201,6 +301,7 @@ export default function UsersTable() {
                         variant={"ghost"}
                         colorScheme="blue"
                         icon={<ViewIcon />}
+                        onClick={() => openUserInfoModal(user)}
                       />
                     </Tooltip>
 
