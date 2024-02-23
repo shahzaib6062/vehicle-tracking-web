c28ec6fbdf4c1c78b0efe85e344e41549e408d47
Abubakr Muhammad <realchampiontheno1@gmail.com>
2024-02-23 08:08:43 +0500

    remove extra files

--- src/pages/admin/users/[id].jsx
+++ src/pages/admin/users/[id].jsx
@@ -1,6 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { db, firestore } from "../../firebase/firebase";
+import { db, firestore } from "../../../firebase/firebase";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import AuthWrapper from "@/components/authWrapper";
 import { doc } from "firebase/firestore";
